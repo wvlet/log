8a48e7d0f1002f79dc45387ac1a8a707cfc97b01
Taro L. Saito <leo@xerial.org>
2016-04-28 10:55:11 -0700

    Add simple object writer

--- wvlet-core/src/main/scala/wvlet/object/ObjectInput.scala
+++ wvlet-core/src/main/scala/wvlet/object/ObjectInput.scala
@@ -1,7 +1,7 @@
 package wvlet.`object`
 
 import wvlet.core.tablet.{Column, Tablet, TabletWriter}
-import xerial.lens.{ObjectSchema, Primitive, TextType}
+import xerial.lens.{ObjectSchema, Primitive, TextType, TypeConverter}
 
 import scala.reflect.ClassTag
 
@@ -42,15 +42,42 @@ class ObjectWriter[A: ClassTag](name: String, output:TabletWriter) {
   import ObjectWriter._
 
   val schema = ObjectSchema.of[A]
+
+  // TODO Create data conversion operator using Tablet
   val tablet = createTabletOf[A](name)
-
-
 
   def write(record: A) {
     output.writeRecord {
-      
-
-
+      for(p <- schema.parameters) {
+        val v = p.get(record)
+        if (v == null) {
+          output.writeNull
+        }
+        else {
+          p.valueType match {
+            case Primitive.Byte | Primitive.Short | Primitive.Int | Primitive.Long =>
+              TypeConverter.convertToPrimitive(v, Primitive.Long) match {
+                case Some(l) => output.writeLong(l)
+                case None => output.writeNull
+              }
+            case Primitive.Float | Primitive.Double =>
+              TypeConverter.convertToPrimitive(v, Primitive.Double) match {
+                case Some(d) => output.writeDouble(d)
+                case None => output.writeNull
+              }
+            case Primitive.Boolean =>
+              TypeConverter.convertToPrimitive(v, Primitive.Boolean) match {
+                case Some(b) => output.writeBoolean(b)
+                case None => output.writeNull
+              }
+            case Primitive.Char | TextType.String | TextType.File | TextType.Date =>
+              output.writeString(v.toString)
+            case other =>
+              // TODO support Array, Map, etc.
+              output.writeString(other.toString())
+          }
+        }
+      }
     }
   }
 
