c987d46d6ac23827e6f93346ba58949137507fa7
Taro L. Saito <leo@xerial.org>
2016-07-22 11:47:03 -0700

    Add MBean access test

--- wvlet-jmx/src/test/scala/wvlet/jmx/JMXAgentTest.scala
+++ wvlet-jmx/src/test/scala/wvlet/jmx/JMXAgentTest.scala
@@ -1,7 +1,9 @@
 package wvlet.jmx
 
+import javax.management.ObjectName
 import javax.management.remote.{JMXConnectorFactory, JMXServiceURL}
 
+import com.sun.org.glassfish.gmbal.AMXClient
 import wvlet.test.WvletSpec
 
 /**
@@ -10,7 +12,7 @@ import wvlet.test.WvletSpec
 class JMXAgentTest extends WvletSpec {
 
   "JMXAgent" should {
-    "start jmx registry" in {
+    "find jmx registry" in {
       val agent = new JMXAgent(new JMXConfig())
       val url = new JMXServiceURL(agent.serviceUrl)
       val connector = JMXConnectorFactory.connect(url)
@@ -18,7 +20,9 @@ class JMXAgentTest extends WvletSpec {
 
       val connection = connector.getMBeanServerConnection()
       connection.getMBeanCount.toInt shouldBe > (0)
+      val m = connection.getMBeanInfo(new ObjectName("java.lang:type=OperatingSystem"))
+      m shouldNot be (null)
+      info(m)
     }
-
   }
 }
